a6c8ad58f1a3465798fe7e3bc8c739dce47a5cf0
Sylvain Gris <sgris@melistechnology.com>
2017-04-10 11:09:38 +0200

    update tests folder

---
+++ test/MelisFrontTest/Controller/MelisFrontControllerTest.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Melis Technology (http://www.melistechnology.com)
+ *
+ * @copyright Copyright (c) 2016 Melis Technology (http://www.melistechnology.com)
+ *
+ */
+
+namespace MelisFrontTest\Controller;
+
+use MelisCore\ServiceManagerGrabber;
+use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
+class MelisFrontControllerTest extends AbstractHttpControllerTestCase
+{
+    protected $traceError = false;
+    protected $sm;
+    protected $method = 'save';
+
+    public function setUp()
+    {
+        $this->sm  = new ServiceManagerGrabber();
+    }
+
+    
+
+    public function getPayload($method)
+    {
+        return $this->sm->getPhpUnitTool()->getPayload('MelisFront', $method);
+    }
+
+    /**
+     * START ADDING YOUR TESTS HERE
+     */
+
+    public function testBasicMelisFrontTestSuccess()
+    {
+        $this->assertEquals("equalvalue", "equalvalue");
+    }
+
+    public function testBasicMelisFrontTestError()
+    {
+        $this->assertEquals("supposed-to", "display-an-error");
+    }
+
+
+
+}
+
