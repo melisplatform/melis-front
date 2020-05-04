9373532dec32ba590caede3b51d786f552c160d4
Ralph Bryan Brioso <bbrioso@melistechnology.com>
2020-05-04 17:48:11 +0800

    Page title in digits issue

--- src/Listener/MelisFrontSEOMetaPageListener.php
+++ src/Listener/MelisFrontSEOMetaPageListener.php
@@ -21,55 +21,55 @@ use MelisCore\Listener\MelisGeneralListener;
  */
 class MelisFrontSEOMetaPageListener extends MelisGeneralListener implements ListenerAggregateInterface
 {
-    public function attach(EventManagerInterface $events, $priority = 1)
-    {
-        $callBackHandler = $events->attach(
-        	MvcEvent::EVENT_FINISH, 
-        	function(MvcEvent $e){
-        		
-        	    // Get route match to know if we are displaying in back or front
-            	$routeMatch = $e->getRouteMatch();
+	public function attach(EventManagerInterface $events, $priority = 1)
+	{
+		$callBackHandler = $events->attach(
+			MvcEvent::EVENT_FINISH, 
+			function(MvcEvent $e){
+				
+				// Get route match to know if we are displaying in back or front
+				$routeMatch = $e->getRouteMatch();
 
-            	// AssetManager, we don't want listener to be executed if it's not a php code
-            	$uri = $_SERVER['REQUEST_URI'];
-        		preg_match('/.*\.((?!php).)+(?:\?.*|)$/i', $uri, $matches, PREG_OFFSET_CAPTURE);
-        		if (count($matches) > 1)
-        		    return;
+				// AssetManager, we don't want listener to be executed if it's not a php code
+				$uri = $_SERVER['REQUEST_URI'];
+				preg_match('/.*\.((?!php).)+(?:\?.*|)$/i', $uri, $matches, PREG_OFFSET_CAPTURE);
+				if (count($matches) > 1)
+					return;
 
-        		// No routematch, we're not in Melis, no need this listener
-            	if (!$routeMatch)
-            		return;
-            	 
-            	$renderMode = $routeMatch->getParam('renderMode');
+				// No routematch, we're not in Melis, no need this listener
+				if (!$routeMatch)
+					return;
+				
+				$renderMode = $routeMatch->getParam('renderMode');
 
-            	// Only for Melis Front or Back routes
-            	if ($renderMode == 'melis' || $renderMode == 'front')
-            	{
-            		$sm = $e->getApplication()->getServiceManager();
-            	
-            		// Get the response generated
-            		$response = $e->getResponse();
-            		$content = $response->getContent();
-            	
-            		$params = $routeMatch->getParams();
-            		
-            		if (empty($params['idpage']))
-            		    return;
-            		
-            		$idpage = $params['idpage'];
-            	
-            		/**
-            		 * Replace Head and SEO datas automatically
-            		 */
-            		$melisFrontHead = $sm->get('MelisFrontHead');
-            		$newContent = $melisFrontHead->updateTitleAndDescription($idpage, $content);
-            	
-            		// Set the updated content
-            		$response->setContent($newContent);
-            	}
-        	},
-        110);
-        
-        $this->listeners[] = $callBackHandler;
-    }
+				// Only for Melis Front or Back routes
+				if ($renderMode == 'melis' || $renderMode == 'front')
+				{
+					$sm = $e->getApplication()->getServiceManager();
+				
+					// Get the response generated
+					$response = $e->getResponse();
+					$content = $response->getContent();
+				
+					$params = $routeMatch->getParams();
+					
+					if (empty($params['idpage']))
+						return;
+					
+					$idpage = $params['idpage'];
+				
+					/**
+					 * Replace Head and SEO datas automatically
+					 */
+					$melisFrontHead = $sm->get('MelisFrontHead');
+					$newContent = $melisFrontHead->updateTitleAndDescription($idpage, $content);
+					
+					// Set the updated content
+					$response->setContent($newContent);
+				}
+			},
+		110);
+		
+		$this->listeners[] = $callBackHandler;
+	}
 }
