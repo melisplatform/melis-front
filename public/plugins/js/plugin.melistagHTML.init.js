fa43de641d7c4e6b36051a4cefb0ec20c16019df
bbrioso <rbbrioso28@gmail.com>
2021-04-28 18:53:09 +0800

    Enhancing page edtion load performance

--- public/plugins/js/plugin.melistagHTML.init.js
+++ public/plugins/js/plugin.melistagHTML.init.js
@@ -1,42 +1,46 @@
-function melistagHTML_init(idPlugin){
-    // declaring parameters variable for old / cross browser compatability
-    if(typeof idPlugin === "undefined") idPlugin = null;
+function melistagHTML_init(idPlugin) {
+	// declaring parameters variable for old / cross browser compatability
+	if (typeof idPlugin === "undefined") idPlugin = null;
 
-	var tinyMceOption = {templates: '/melis/MelisCms/PageEdition/getTinyTemplates?idPage=' + melisActivePageId};
-	
-	if(idPlugin != null){
-		tinyMceOption.setup = 'melistagHTML_savePlugin';
+	var tinyMceOption = {
+		templates:
+			"/melis/MelisCms/PageEdition/getTinyTemplates?idPage=" +
+			melisActivePageId,
+	};
+
+	if (idPlugin != null) {
+		tinyMceOption.setup = "melistagHTML_savePlugin";
 	}
-	
-	var idPlugin = idPlugin !== null ?  "#"+idPlugin : "div.html-editable";
-	
+
+	var idPlugin = idPlugin !== null ? "#" + idPlugin : "div.html-editable";
+
 	// Editor the will use for Pages
 	melisTinyMCE.createTinyMCE("html", idPlugin, tinyMceOption);
 }
 
-function melistagHTML_savePlugin(editor){
-	editor.on("init",function(ed) {
-		$("#"+editor.id).trigger("blur");
-    });
+function melistagHTML_savePlugin(editor) {
+	editor.on("init", function(ed) {
+		$("#" + editor.id).trigger("blur");
+	});
 }
 
 // run this function when you click out of the tinymce
 $("body").on("blur", "div.html-editable", function() {
 	// get all data-attributes from the clicked
 	var currentdata = $(this).data();
-	
+
 	// get the content of the active tinyMCE editor.
-	var content = tinyMCE.activeEditor.getContent({format : 'html'});
-	
+	var content = tinyMCE.activeEditor.getContent({ format: "html" });
+
 	var data = {
 		melisPluginName: currentdata.plugin,
 		melisPluginTag: currentdata.melisTag,
-		melisPluginId : currentdata.tagId,
+		melisPluginId: currentdata.tagId,
 		tagType: currentdata.tagType,
-		tagId : currentdata.tagId,
-		tagValue : content,
+		tagId: currentdata.tagId,
+		tagValue: content,
 	};
-    melisPluginEdition.calcFrameHeight();
+	melisPluginEdition.calcFrameHeight();
 	melisPluginEdition.savePluginUpdate(data, currentdata.siteModule);
 });
 
--- public/plugins/js/plugin.melistagMEDIA.init.js
+++ public/plugins/js/plugin.melistagMEDIA.init.js
@@ -1,45 +1,47 @@
-function melistagMEDIA_init(idPlugin){
-    // declaring parameters variable for old / cross browser compatability
-    if(typeof idPlugin === "undefined") idPlugin = null;
+function melistagMEDIA_init(idPlugin) {
+	// declaring parameters variable for old / cross browser compatability
+	if (typeof idPlugin === "undefined") idPlugin = null;
 
-	var tinyMceOption = new Array;
-	
-	if(idPlugin != null){
-		tinyMceOption = {setup: 'melistagMEDIA_savePlugin'};
+	var tinyMceOption = {
+		templates:
+			"/melis/MelisCms/PageEdition/getTinyTemplates?idPage=" +
+			melisActivePageId,
+	};
+
+	if (idPlugin != null) {
+		tinyMceOption.setup = "melistagMEDIA_savePlugin";
 	}
-	
-	var idPlugin = idPlugin !== null ?  "#"+idPlugin : "div.media-editable";
+
+	var idPlugin = idPlugin !== null ? "#" + idPlugin : "div.media-editable";
 
 	// Editor the will use for Pages
 	melisTinyMCE.createTinyMCE("media", idPlugin, tinyMceOption);
 }
 
-function melistagMEDIA_savePlugin(editor){
-	editor.on("init",function(ed) {
-		$("#"+editor.id).trigger("blur");
-    });
+function melistagMEDIA_savePlugin(editor) {
+	editor.on("init", function(ed) {
+		$("#" + editor.id).trigger("blur");
+	});
 }
 
 // run this function when you click out of the tinymce
-$("body").on("blur", "div.media-editable", function(){
-	
+$("body").on("blur", "div.media-editable", function() {
 	// get all data-attributes from the clicked
 	var currentdata = $(this).data();
-	
+
 	// get the content of the active tinyMCE editor.
-	var content = tinyMCE.activeEditor.getContent({format : 'html'});
-	
+	var content = tinyMCE.activeEditor.getContent({ format: "html" });
+
 	var data = {
 		melisPluginName: currentdata.plugin,
 		melisPluginTag: currentdata.melisTag,
-		melisPluginId : currentdata.tagId,
+		melisPluginId: currentdata.tagId,
 		tagType: currentdata.tagType,
-		tagId : currentdata.tagId,
-		tagValue : content,
-    };
-    melisPluginEdition.calcFrameHeight();
-    melisPluginEdition.savePluginUpdate(data, currentdata.siteModule);
+		tagId: currentdata.tagId,
+		tagValue: content,
+	};
+	melisPluginEdition.calcFrameHeight();
+	melisPluginEdition.savePluginUpdate(data, currentdata.siteModule);
 });
-
 
 melistagMEDIA_init();
