f5501ec49422eb3b8cb82840264a9b79dbfe7a88
jerremeirago <jrago@melistechnology.com>
2021-08-27 09:55:17 +0000

    added SameSite attribute in gdpr banner cookie

--- public/plugins/js/plugin.melisGdprBanner.init.js
+++ public/plugins/js/plugin.melisGdprBanner.init.js
@@ -30,7 +30,7 @@ window.MelisGdprBanner = (function () {
          * src: https://zellwk.com/blog/looping-through-js-objects/
          */
         options = Object.entries({defaultOptions,options});
-        var updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+        var updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value) + "; SameSite=Lax";
         for (const [key, value] of options) {
             updatedCookie += "; " + key + "=" + value;
         }
