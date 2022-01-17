b09b9f85f41f9fd3dfc443bdef43d936c42beca3
Maria Teresa Pomar <mpomar@melistechnology.com>
2022-01-17 17:57:17 +0800

    checks for the specific 404 page of the locale

--- src/Listener/MelisFront404CatcherListener.php
+++ src/Listener/MelisFront404CatcherListener.php
@@ -24,7 +24,7 @@ class MelisFront404CatcherListener extends MelisGeneralListener implements Liste
         $callBackHandler = $events->attach(
         	MvcEvent::EVENT_FINISH, 
         	function(MvcEvent $e){
-        	    
+
         	    // AssetManager, we don't want listener to be executed if it's not a php code
         	    $uri = $_SERVER['REQUEST_URI'];
         	    preg_match('/.*\.((?!php).)+(?:\?.*|)$/i', $uri, $matches, PREG_OFFSET_CAPTURE);
@@ -47,32 +47,55 @@ class MelisFront404CatcherListener extends MelisGeneralListener implements Liste
         		    $site301 = $sm->get('MelisEngineTableSite301');
         		    $site301Datas = $site301->getEntryByField('s301_old_url', $uri->getPath());
         		    $url = null;
-        		    if (!empty($site301Datas->count()))
-        		    {
+        		    if (!empty($site301Datas->count())) {
         		        
         		        $newUrlHost = array();
         		        
-        		        foreach($site301Datas as $site301Data){
+        		        foreach ($site301Datas as $site301Data) {
         		            
-        		                if(!empty( $site301Data->s301_site_id)){
+        		                if (!empty( $site301Data->s301_site_id)) {
         		                    $siteDomain  = $siteDomainTable->getEntryByField('sdom_site_id', $site301Data->s301_site_id)->current();
         		                     
-        		                    if($siteDomain->sdom_domain == $uri->getHost() && $site301Data->s301_site_id == $siteDomain->sdom_site_id){
+        		                    if ($siteDomain->sdom_domain == $uri->getHost() && $site301Data->s301_site_id == $siteDomain->sdom_site_id) {
         		                        $url = $site301Data->s301_new_url;
         		                    }
-        		                }else{
+        		                } else {
         		                    $url = $site301Data->s301_new_url;
         		                }
         		        }
-        		    }else{
+        		    } else {
+
         		        // check for site 404
         		        $siteDomain  = $siteDomainTable->getEntryByField('sdom_domain', $uri->getHost())->current();
-        		        
-        		        if(!empty($siteDomain)){
-        		            
-        		            $site404Table = $sm->get('MelisEngineTableSite404');
-        		            $site404Data = $site404Table->getEntryByField('s404_site_id', $siteDomain->sdom_site_id)->current();
-        		            
+                               		        
+        		        if (!empty($siteDomain)) {
+                            
+                            //check for the locale in the url
+                            $uriArr = explode('/', $uri->getPath());
+                            if ($uriArr) {
+                                $locale = $uriArr[1];
+                                /**
+                                 * Make a locale
+                                 */
+                                $siteLangLocale = $locale . '_' . strtoupper($locale);
+                                $langSrv = $sm->get('MelisEngineLang');
+                                $langData = $langSrv->getLangDataByLangLocale($siteLangLocale);
+
+                                if (!empty($langData)) {
+                                    $langId = $langData->lang_cms_id;
+
+                                    //check if there is a 404 page for a specific language
+                                    $siteService = $sm->get('MelisEngineSiteService');
+                                    $site404Data = $siteService->get404PageBySiteIdAndLangId($siteDomain->sdom_site_id, $langId);
+                                }                               
+                            }
+                            
+                            //if no 404 data for a specific locale or no locale in the url, get the first 404 page given the site id
+                            if (!isset($site404Data)) {                              
+                                $site404Table = $sm->get('MelisEngineTableSite404');
+                                $site404Data = $site404Table->getEntryByField('s404_site_id', $siteDomain->sdom_site_id)->current();
+                            }
+        		                    		                  		            
         		            if(!empty($site404Data)){
         		                
         		                $melisTree = $sm->get('MelisEngineTree');
