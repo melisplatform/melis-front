cc34e50162c51464c99a70fe2b27d08b288de742
Maria Teresa Pomar <mpomar@melistechnology.com>
2022-04-04 19:57:21 +0800

    changed deprecated ArraySerializable to ArraySerializableHydrator and updated other functions affected by php 8

--- src/Controller/Plugin/MelisFrontSearchResultsPlugin.php
+++ src/Controller/Plugin/MelisFrontSearchResultsPlugin.php
@@ -241,7 +241,7 @@ class MelisFrontSearchResultsPlugin extends MelisTemplatingPlugin
                 } else {
 
                     // validate the forms and send back an array with errors by tabs
-                    $post = get_object_vars($request->getPost());
+                    $post = $request->getPost()->toArray();
                     $success = false;
                     $form->setData($post);
 
