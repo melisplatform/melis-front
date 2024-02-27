17722dce6968a8d169d7dbbb88d67091537c580f
jzabate <jzabate@melistechnology.com>
2024-02-27 17:29:18 +0800

    check issue on page edition tinymce drag and drop of mini template

--- public/plugins/js/plugin.melistagHTML.init.js
+++ public/plugins/js/plugin.melistagHTML.init.js
@@ -19,6 +19,7 @@ function melistagHTML_init(idPlugin) {
 function melistagHTML_savePlugin(editor) {
 	editor.on("init", function(ed) {
 		$("#" + editor.id).trigger("blur");
+		console.log("melis-front plugin.melistagHTML.init.js #+editor.id: ", "#"+editor.id);
 	});
 }
 
