6a81814e72fb3508a399702f072fd25cd06dc8e7
Cris <crismakabenta@gmail.com>
2024-12-06 19:59:11 +0800

    added parameter to check if its from attribute

--- src/View/Helper/MelisSiteTranslationHelper.php
+++ src/View/Helper/MelisSiteTranslationHelper.php
@@ -28,9 +28,10 @@ class MelisSiteTranslationHelper extends AbstractHelper
      * @param string $key
      * @param int $langId
      * @param int $siteId
+     * @param boolean $isInAttribute - check if the translations is put inside an attribute like placeholder or title
      * @return string
      */
-    public function __invoke($key, $langId, $siteId)
+    public function __invoke($key, $langId, $siteId, $isInAttribute = false)
     {
         $siteTransSrv = $this->serviceManager->get('MelisSiteTranslationService');
 
@@ -46,16 +47,24 @@ class MelisSiteTranslationHelper extends AbstractHelper
         }
         $str = $cacheData[$langId][$key] ?? $key;
 
-
-        if(!empty($this->getView()->renderMode)){
-            if($this->getView()->renderMode != 'front'){
-                $str = '<span title="'.$key.'">'.$str.'</span>';
-            }
-        }else{//try to check uri if we are in BO
-            $uri = $_SERVER['REQUEST_URI'];
-            $uri = explode('/', $uri);
-            if(in_array('renderMode', $uri)){
-                $str = '<span title="'.$key.'">'.$str.'</span>';
+        /**
+         * We modify only the translation if we are not inside
+         * an attribute since html attribute don't accept
+         * html tag like span
+         */
+        if(!$isInAttribute) {
+            if (!empty($this->getView()->renderMode)) {
+                if ($this->getView()->renderMode != 'front') {
+                    //return translation with a span so they can see the key when they hover the text
+                    $str = '<span title="' . $key . '">' . $str . '</span>';
+                }
+            } else {//try to check uri if we are in BO
+                $uri = $_SERVER['REQUEST_URI'];
+                $uri = explode('/', $uri);
+                if (in_array('renderMode', $uri)) {
+                    //return translation with a span so they can see the key when they hover the text
+                    $str = '<span title="' . $key . '">' . $str . '</span>';
+                }
             }
         }
 
