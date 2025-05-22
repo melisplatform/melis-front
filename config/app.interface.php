f59bd002cba652215c1c3274f7751f51bccb0bb9
jzabate <jzabate@melistechnology.com>
2025-05-22 17:24:24 +0800

    changes on melis-container,  melis-container-buttons and additional layout templates

--- config/app.interface.php
+++ config/app.interface.php
@@ -38,8 +38,8 @@ return [
                                         </div>
                                     </button>',
             ],
-            '2-rows-2-cols-1-col' => [
-                'template' => 'MelisFront/dnd-2-rows-3-cols-1-col-tpl',
+            '2-rows-1-3-col-center-w-bottom-1-col' => [
+                'template' => 'MelisFront/dnd-2-rows-1-3-col-center-w-bottom-1-col-tpl',
                 'html-button-icon' => '<button class="column-icon top-1-3-col-center">
                                         <div class="icon-container">
                                             <div class="icon-row justify-content-center">
@@ -47,6 +47,19 @@ return [
                                             </div>
                                             <div class="icon-row">
                                                 <div class="icon-col-bg icon-col-12 bg-white"></div>
+                                            </div>
+                                        </div>
+                                    </button>',
+            ],
+            '2-rows-top1-col-w-bottom-1-3-col-center' => [
+                'template' => 'MelisFront/dnd-2-rows-top1-col-w-bottom-1-3-col-center-tpl',
+                'html-button-icon' => '<button class="column-icon top-1-col-w-bottom-1-3-center">
+                                        <div class="icon-container">
+                                            <div class="icon-row">
+                                                <div class="icon-col-bg icon-col-12 bg-white"></div>
+                                            </div>
+                                            <div class="icon-row justify-content-center">
+                                                <div class="icon-col-bg icon-col-4 bg-white"></div>
                                             </div>
                                         </div>
                                     </button>',
