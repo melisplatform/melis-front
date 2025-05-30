6d8d98779d02cc26697d55fdd81ed7e19b0432e6
jzabate <jzabate@melistechnology>
2025-05-30 19:45:36 +0800

    changes on .dnd-layout-buttons and added the custom column layouts

--- config/app.interface.php
+++ config/app.interface.php
@@ -63,6 +63,88 @@ return [
                                         </div>
                                     </button>',
             ],
+            '3-cols-1-col-left-w-2-cols-right' => [
+                'template' => 'MelisFront/dnd-3-cols-1-col-left-w-2-cols-right-tpl',
+                'html-button-icon' => '<button class="column-icon half-1-col-left-w-right-2-col">
+                                        <div class="icon-container">
+                                            <div class="icon-row">
+                                                <div class="icon-col-6 icon-col-bg bg-white">
+                                                </div>
+                                                <div class="icon-col-6">
+                                                    <div class="icon-row">
+                                                        <div class="icon-col-bg icon-col-12 bg-white"></div>
+                                                    </div>
+                                                    <div class="icon-row">
+                                                        <div class="icon-col-bg icon-col-12 bg-white"></div>
+                                                    </div>
+                                                </div>	
+                                            </div>
+                                        </div>
+                                    </button>'
+            ],
+            '3-cols-2-cols-left-w-1-col-right' => [
+                'template' => 'MelisFront/dnd-3-cols-2-cols-left-w-1-col-right-tpl',
+                'html-button-icon' => '<button class="column-icon left-2-col-w-right-1-col">
+                                        <div class="icon-container">
+                                            <div class="icon-row">
+                                                <div class="icon-col-6">
+                                                    <div class="icon-row">
+                                                        <div class="icon-col-bg icon-col-12 bg-white"></div>
+                                                    </div>
+                                                    <div class="icon-row">
+                                                        <div class="icon-col-bg icon-col-12 bg-white"></div>
+                                                    </div>
+                                                </div>
+                                                <div class="icon-col-6 icon-col-bg bg-white">
+                                                </div>	
+                                            </div>
+                                        </div>
+                                    </button>'
+            ],
+            '4-cols-1-col-left-w-3-cols-right' => [
+                'template' => 'MelisFront/dnd-4-cols-1-col-left-w-3-cols-right-tpl',
+                'html-button-icon' => '<button class="column-icon half-1-col-left-w-right-3-col">
+                                        <div class="icon-container">
+                                            <div class="icon-row">
+                                                <div class="icon-col-6 icon-col-bg bg-white">
+                                                </div>
+                                                <div class="icon-col-6">
+                                                    <div class="icon-row">
+                                                        <div class="icon-col-bg icon-col-12 bg-white"></div>
+                                                    </div>
+                                                    <div class="icon-row">
+                                                        <div class="icon-col-bg icon-col-12 bg-white"></div>
+                                                    </div>
+                                                    <div class="icon-row">
+                                                        <div class="icon-col-bg icon-col-12 bg-white"></div>
+                                                    </div>
+                                                </div>	
+                                            </div>
+                                        </div>
+                                    </button>'
+            ],
+            '4-cols-3-cols-left-w-1-col-right' => [
+                'template' => 'MelisFront/dnd-4-cols-3-cols-left-w-1-col-right-tpl',
+                'html-button-icon' => '<button class="column-icon left-3-col-w-half-1-col-right">
+                                        <div class="icon-container">
+                                            <div class="icon-row">
+                                                <div class="icon-col-6">
+                                                    <div class="icon-row">
+                                                        <div class="icon-col-bg icon-col-12 bg-white"></div>
+                                                    </div>
+                                                    <div class="icon-row">
+                                                        <div class="icon-col-bg icon-col-12 bg-white"></div>
+                                                    </div>
+                                                    <div class="icon-row">
+                                                        <div class="icon-col-bg icon-col-12 bg-white"></div>
+                                                    </div>
+                                                </div>
+                                                <div class="icon-col-6 icon-col-bg bg-white">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </button>'
+            ],            
             '2-rows-1-3-col-center-w-bottom-1-col' => [
                 'template' => 'MelisFront/dnd-2-rows-1-3-col-center-w-bottom-1-col-tpl',
                 'html-button-icon' => '<button class="column-icon top-1-3-col-center" title="Top row 1/3 column centered, bottom row one column">
