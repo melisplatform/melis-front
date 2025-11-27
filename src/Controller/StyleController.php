05b98da02f27841f62097d62911ca03aba877e49
rbbrioso28 <rbbrioso28@gmail.com>
2025-11-27 17:19:07 +0800

    grid col padding reset to zero

--- src/Controller/StyleController.php
+++ src/Controller/StyleController.php
@@ -23,48 +23,46 @@ class StyleController extends MelisAbstractActionController
 
         $response->getHeaders()
             ->addHeaderLine('Cache-Control', 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0')
-            ->addHeaderLine('Pragma'       , 'no-cache')
-            ->addHeaderLine('Content-Type' , 'text/css;charset=UTF-8');
+            ->addHeaderLine('Pragma', 'no-cache')
+            ->addHeaderLine('Content-Type', 'text/css;charset=UTF-8');
 
-        $css     = '[class^="plugin-width"] {float: left;margin: 0;}';
+        $css     = '[class^="plugin-width"] {float: left;margin: 0;}' . PHP_EOL;
+        $css     .= '[class*="dnd-plugins-col-"] {padding-left: 0px; padding-right: 0px;}' . PHP_EOL;
         $pageId  = (int) $this->params()->fromQuery('idpage');
 
         $pageSvc = $this->getServiceManager()->get('MelisEnginePage');
         $data    = $pageSvc->getDatasPage($pageId);
 
-        if($data) {
+        if ($data) {
 
             $data = $data->getMelisPageTree()->page_content;
 
             $desktop = array(100 => 100);
-            if(preg_match_all('/width_desktop=\"(.*?)\"/', $data, $matches)) {
-                if(isset($matches[1]) && is_array($matches[1])) {
-                    foreach($matches[1] as $widths)
+            if (preg_match_all('/width_desktop=\"(.*?)\"/', $data, $matches)) {
+                if (isset($matches[1]) && is_array($matches[1])) {
+                    foreach ($matches[1] as $widths)
                         $desktop[$widths] = $widths;
                 }
             }
-            $css .= $this->getMediaQuery(null, 981, $this->getClasses($desktop,'lg'), 'min').PHP_EOL;
+            $css .= $this->getMediaQuery(null, 981, $this->getClasses($desktop, 'lg'), 'min') . PHP_EOL;
 
             $tablet = array(100 => 100);
-            if(preg_match_all('/width_tablet=\"(.*?)\"/', $data, $matches)) {
-                if(isset($matches[1]) && is_array($matches[1])) {
-                    foreach($matches[1] as $widths)
+            if (preg_match_all('/width_tablet=\"(.*?)\"/', $data, $matches)) {
+                if (isset($matches[1]) && is_array($matches[1])) {
+                    foreach ($matches[1] as $widths)
                         $tablet[$widths] = $widths;
                 }
             }
-            $css .= $this->getMediaQuery(481, 980, $this->getClasses($tablet, 'md')).PHP_EOL;
+            $css .= $this->getMediaQuery(481, 980, $this->getClasses($tablet, 'md')) . PHP_EOL;
 
             $mobile = array(100 => 100);
-            if(preg_match_all('/width_mobile=\"(.*?)\"/', $data, $matches)) {
-                if(isset($matches[1]) && is_array($matches[1])) {
-                    foreach($matches[1] as $widths)
+            if (preg_match_all('/width_mobile=\"(.*?)\"/', $data, $matches)) {
+                if (isset($matches[1]) && is_array($matches[1])) {
+                    foreach ($matches[1] as $widths)
                         $mobile[$widths] = $widths;
-
-
                 }
             }
-            $css .= $this->getMediaQuery(null, 480, $this->getClasses($mobile,'xs')).PHP_EOL;
-
+            $css .= $this->getMediaQuery(null, 480, $this->getClasses($mobile, 'xs')) . PHP_EOL;
         }
 
         $response->setContent($css);
@@ -75,7 +73,6 @@ class StyleController extends MelisAbstractActionController
         $view->content = $response->getContent();
 
         return $view;
-
     }
 
     public function pluginWidthsAction()
@@ -84,15 +81,15 @@ class StyleController extends MelisAbstractActionController
 
         $response->getHeaders()
             ->addHeaderLine('Cache-Control', 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0')
-            ->addHeaderLine('Pragma'       , 'no-cache')
-            ->addHeaderLine('Content-Type' , 'text/css;charset=UTF-8');
+            ->addHeaderLine('Pragma', 'no-cache')
+            ->addHeaderLine('Content-Type', 'text/css;charset=UTF-8');
         // text/css
 
         $css = null;
 
-        $css = $this->getMediaQuery(null, 480, $this->getClasses('xs')).PHP_EOL;
-        $css .= $this->getMediaQuery(481, 980, $this->getClasses('md')).PHP_EOL;
-        $css .= $this->getMediaQuery(null, 981, $this->getClasses('lg'), 'min').PHP_EOL;
+        $css = $this->getMediaQuery(null, 480, $this->getClasses('xs')) . PHP_EOL;
+        $css .= $this->getMediaQuery(481, 980, $this->getClasses('md')) . PHP_EOL;
+        $css .= $this->getMediaQuery(null, 981, $this->getClasses('lg'), 'min') . PHP_EOL;
         $response->setContent($css);
 
         $view = new ViewModel();
@@ -106,14 +103,13 @@ class StyleController extends MelisAbstractActionController
     private function getMediaQuery($minWidth, $maxWidth, $classes = null, $type = 'max')
     {
         $query  = null;
-        if(!is_null($minWidth)) {
-            $query  = "@media (min-width: {$minWidth}px) and (max-width: {$maxWidth}px)".PHP_EOL;
-        }
-        else {
-            $query .= "@media only screen and ({$type}-width: {$maxWidth}px)".PHP_EOL;
+        if (!is_null($minWidth)) {
+            $query  = "@media (min-width: {$minWidth}px) and (max-width: {$maxWidth}px)" . PHP_EOL;
+        } else {
+            $query .= "@media only screen and ({$type}-width: {$maxWidth}px)" . PHP_EOL;
         }
 
-        $query .= "{".PHP_EOL;
+        $query .= "{" . PHP_EOL;
         $query .= $classes;
         $query .= "}";
 
@@ -123,34 +119,31 @@ class StyleController extends MelisAbstractActionController
     private function getClasses($widths = array(), $prefix = null)
     {
         $classes = null;
-        $prefix  = !is_null($prefix) ? '-'.$prefix : null;
+        $prefix  = !is_null($prefix) ? '-' . $prefix : null;
 
         // load the default CSS
-        if(!$widths) {
-            for($x = 1; $x <= 100.01; $x+=0.01) {
-                $value     = number_format( round( (float) $x, 2), 2, '.', ',');
-                $className = number_format( round( (float) $x, 2), 2, '-', ',');
+        if (!$widths) {
+            for ($x = 1; $x <= 100.01; $x += 0.01) {
+                $value     = number_format(round((float) $x, 2), 2, '.', ',');
+                $className = number_format(round((float) $x, 2), 2, '-', ',');
 
-                $classes .= "    .plugin-width{$prefix}-{$className} {".PHP_EOL;
-                $classes .= "         width: {$value}%;".PHP_EOL;
-                $classes .= "     }".PHP_EOL;
+                $classes .= "    .plugin-width{$prefix}-{$className} {" . PHP_EOL;
+                $classes .= "         width: {$value}%;" . PHP_EOL;
+                $classes .= "     }" . PHP_EOL;
             }
-        }
-        else {
+        } else {
             // load specific CSS
-            foreach($widths as $width) {
-                $value     = number_format( round( (float) $width, 2), 2, '.', ',');
-                $className = number_format( round( (float) $width, 2), 2, '-', ',');
+            foreach ($widths as $width) {
+                $value     = number_format(round((float) $width, 2), 2, '.', ',');
+                $className = number_format(round((float) $width, 2), 2, '-', ',');
 
-                $classes .= "    .plugin-width{$prefix}-{$className} {".PHP_EOL;
-                $classes .= "         width: {$value}%;".PHP_EOL;
-                $classes .= "     }".PHP_EOL;
+                $classes .= "    .plugin-width{$prefix}-{$className} {" . PHP_EOL;
+                $classes .= "         width: {$value}%;" . PHP_EOL;
+                $classes .= "     }" . PHP_EOL;
             }
         }
 
 
         return $classes;
     }
-
-
 }
