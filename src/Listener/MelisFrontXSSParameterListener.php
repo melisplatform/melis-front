136a116080921fc3daa74b55648d486286d0b2bd
Cris <crismakabenta@gmail.com>
2026-06-02 18:54:45 +0800

    Fixed xss problem on uri

--- src/Listener/MelisFrontXSSParameterListener.php
+++ src/Listener/MelisFrontXSSParameterListener.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 /**
  * Melis Technology (http://www.melistechnology.com)
@@ -14,44 +14,56 @@ use Laminas\EventManager\ListenerAggregateInterface;
 use Laminas\Mvc\MvcEvent;
 use MelisCore\Listener\MelisGeneralListener;
 
-/**
- * Site 404 catcher listener
- */
 class MelisFrontXSSParameterListener extends MelisGeneralListener implements ListenerAggregateInterface
 {
+    /**
+     * Request keys that can be coerced into front-end asset URLs by the
+     * MelisTemplatingPlugin config-merge. None of these are legitimate
+     * user-supplied parameters, so they are always removed.
+     */
+    private const BLOCKED_ASSET_KEYS = ['js', 'css', 'files'];
+
     public function attach(EventManagerInterface $events, $priority = 1)
     {
         $callBackHandler = $events->attach(
-            MvcEvent::EVENT_ROUTE, 
+            MvcEvent::EVENT_ROUTE,
             function(MvcEvent $e){
-                
+
                 // AssetManager, we don't want listener to be executed if it's not a php code
                 $uri = $_SERVER['REQUEST_URI'];
                 preg_match('/.*\.((?!php).)+(?:\?.*|)$/i', $uri, $matches, PREG_OFFSET_CAPTURE);
                 if (count($matches) > 1)
                     return;
-                
+
                 $request = $e->getRequest();
                 $GetParameters = $request->getQuery();
-            
+
+                foreach ([$GetParameters, $request->getPost()] as $params) {
+                    foreach (self::BLOCKED_ASSET_KEYS as $key) {
+                        if ($params->offsetExists($key)) {
+                            $params->offsetUnset($key);
+                        }
+                    }
+                }
+
                 foreach ($GetParameters as $key => $value)
                 {
                     if (!is_array($value))
                         $request->getQuery()->set($key, htmlspecialchars(htmlentities($value), ENT_QUOTES, 'UTF-8'));
                     else
                     {
-                    
+
                         array_walk_recursive($value, function (&$val) {
                             $val = htmlentities($val);
                             $val = htmlspecialchars($val, ENT_QUOTES, 'UTF-8');
                         });
-                        
+
                         $request->getQuery()->set($key, $value);
                     }
                 }
             },
-        100);
-        
+            100);
+
         $this->listeners[] = $callBackHandler;
     }
 }
